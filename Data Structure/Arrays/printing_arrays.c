45f532f3a7109a6d4a775da9b0c7208df3e21b93
dipu-ray <dipu34786@gmail.com>
2025-06-28 17:57:42 +0600

    To add printing array

--- Data Structure/Arrays/printing_arrays.c
+++ Data Structure/Arrays/printing_arrays.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
+// Printing array (One dimentional). There array size are your id last one digit and array name are your name.
 int main() {
-    int n;
-    scanf("%d", &n);
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+    int dipu_ray[9];
+    for (int i = 0; i < 9; i++) {
+        scanf("%d", &dipu_ray[i]);
     }
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
+    for (int i = 0; i < 9; i++) {
+        printf("%d ", dipu_ray[i]);
     }
 
     return 0;
