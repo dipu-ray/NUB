a856c47c9d99eb65e3b2f27e93cd12d7b0422c15
dipu-ray <dipu34786@gmail.com>
2025-06-28 18:06:04 +0600

    To add printing array

---
+++ Data Structure/Arrays/main3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Printing array element summations
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
