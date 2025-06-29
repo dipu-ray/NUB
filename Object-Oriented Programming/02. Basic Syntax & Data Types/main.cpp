cfcd90781cd14814d59ffa5d619b30db1c3f2c36
dipu-ray <dipu34786@gmail.com>
2025-06-29 13:02:11 +0600

    To add basic syntax programs

---
+++ Object-Oriented Programming/02. Basic Syntax & Data Types/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// Write a program to declare and initialize variables of different data types and print their values
+int main() {
+    int num = 10;
+    float pi = 3.14;
+    char grade = 'A';
+    string name = "Dipu";
+    bool status = true;
+
+    cout << "Integer: " << num << endl;
+    cout << "Float: " << pi << endl;
+    cout << "Char: " << grade << endl;
+    cout << "String: " << name << endl;
+    cout << "Boolean: " << status << endl;
+
+    return 0;
+}
