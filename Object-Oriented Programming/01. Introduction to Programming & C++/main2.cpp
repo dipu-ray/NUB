781af593feda6b3c9dd9d2356b7e6c40c7809681
dipu-ray <dipu34786@gmail.com>
2025-06-29 12:47:06 +0600

    To add basics c++ programs

---
+++ Object-Oriented Programming/01. Introduction to Programming & C++/main2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Dipu Ray";
+    int age = 22;
+    string university = "XYZ University";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "University: " << university << endl;
+
+    return 0;
+}
