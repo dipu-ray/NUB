073651345123c1f8f47b18737bc872b09824db12
dipu-ray <dipu34786@gmail.com>
2025-08-09 17:37:14 +0600

    To add new program in stacks

---
+++ Data Structure/05. Stack/main2.c
@@ -0,0 +1,36 @@
+// Push another element and display overflow message
+
+#include <stdio.h>
+#define MAX 4
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if(top == MAX - 1) {
+        printf("Stack Overflow! Cannot push %d\n", value);
+    }
+    else {
+        top++;
+        stack[top] = value;
+        printf("Pushed %d to stack.\n", value);
+    }
+}
+
+void display() {
+    if(top == -1) {
+        printf("Stack is empty.\n");
+        return;
+    }
+}
+
+int main() {
+    push(9);
+    push(1);
+    push(4);
+    push(2);
+    push(5); // Overflow
+    display();
+
+    return 0;
+}
